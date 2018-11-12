74c0a577dc0ea4f27a4222bbd871e3f69b6d156f
stk1 <stk1@stk1nomacbook-pro.local>
2018-11-12 17:25:45 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by stk1 on 2018/11/12.
+//  Copyright © 2018 stk1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
